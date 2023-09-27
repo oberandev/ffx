c018e5c22aa88e20a7869a8be7d0bed9b7111329
Hans Hoffman <hans@oberan.dev>
2023-09-27 10:34:19 -0600

    chore: cleanup
    [skip ci]

--- packages/parser-address-geocodio/vite.config.ts
+++ packages/parser-address-geocodio/vite.config.ts
@@ -22,6 +22,6 @@ export default defineConfig({
     },
     environment: "node",
     globals: true,
-    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts}"],
   },
 });
