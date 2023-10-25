1f379028476b6ff401fdf5d0a62e33506b31ff2e
Hans Hoffman <hans@oberan.dev>
2023-10-25 15:45:55 -0600

    ci: fix erroneous curly brackets

--- .buildkite/scripts/test_vitest.sh
+++ .buildkite/scripts/test_vitest.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-if [ "${BUILDKITE_BRANCH}" == "main" ]; then
+if [[ "$BUILDKITE_BRANCH" == "main" ]]; then
   nix-shell --run "pnpm nx affected -t test --base=origin/main~1 --head=origin/main --parallel=5"
 else
-  nix-shell --run "pnpm nx affected -t test --base=origin/main --head=${BUILDKITE_BRANCH} --parallel=5"
+  nix-shell --run "pnpm nx affected -t test --base=origin/main --head=$BUILDKITE_BRANCH --parallel=5"
 fi
