dd962061ea7895879274ca32582e204abd33ddf4
Hans Hoffman <hansjhoffman@users.noreply.github.com>
2023-11-23 09:51:51 -0500

    ci: switch from SemaphoreCI to Buildkite (#51)
    * ci: add buildkite pipeline
    
    * ci: simplify if block
    
    * ci: improve test affected
    
    * ci: fix erroneous curly brackets
    
    * ci: try docker setup
    
    * ci: use latest version of docker plugin
    
    * ci: what directory am I
    
    * ci: spacing issue
    
    * ci: show me your files
    
    * ci: I know you’re there
    
    * ci: local local
    
    * ci: screw it full command
    
    * ci: ok now without docker
    
    * ci: try fs cache
    
    * ci: try all caching
    
    * ci: oops install deps on each step
    
    * ci: add compression
    
    * ci: fix missing pnpm
    
    * ci: fix head
    
    * ci: try inline commands
    
    * ci: switch back to script files
    
    * ci: more flexible bin bash
    
    * ci: move script dir
    
    * docs: replace build status badge with buildkite’s
    
    * ci: remove file extensions
    
    * ci: rename to match others
    
    * style: format file
    
    ---------
    
    Co-authored-by: Hans Hoffman <hans@oberan.dev>

--- shell.nix
+++ shell.nix
@@ -2,9 +2,5 @@ let
   sources = import ./nix/sources.nix { };
   pkgs = import sources.nixpkgs { };
 in pkgs.mkShell {
-  buildInputs = [
-    pkgs.nixfmt
-    pkgs.nodejs_20
-    pkgs.nodePackages.pnpm
-  ];
+  buildInputs = [ pkgs.nixfmt pkgs.nodejs_20 pkgs.nodePackages.pnpm ];
 }
